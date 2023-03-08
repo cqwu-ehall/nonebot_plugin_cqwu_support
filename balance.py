74e4f9ceaaa3a83862787cf2d5e7cd43b4e9487a
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2023-03-09 07:46:06 +0800

    :bug: Fix balance

--- balance.py
+++ balance.py
@@ -18,12 +18,14 @@ async def handle_first_receive(event: Union[GroupMessageEvent, PrivateMessageEve
     if not cqwu_data.has_user(int(event.user_id)):
         await cqwu_balance.finish("⚠️请先使用命令 /cqwu_login 登录网上办事大厅。")
     client = cqwu_data.get_user(int(event.user_id))
+    text = None
     try:
         text = await get_balance(client)
-        await cqwu_balance.finish(f"你的校园卡余额为：{text}")
     except UsernameOrPasswordError:
         await cqwu_balance.finish("⚠️查询失败，用户名或密码错误，请先使用命令 /cqwu_login 重新登录")
     except NeedCaptchaError:
         await cqwu_balance.finish("⚠️查询失败，需要验证码，请先正确登录一次网上办事大厅")
     except Exception as e:
         await cqwu_balance.finish(f"⚠️查询失败，请稍后重试，{type(e)}")
+    if text:
+        await cqwu_balance.finish(f"你的校园卡余额为：{text}")
