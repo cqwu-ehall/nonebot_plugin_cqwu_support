ee303548d1ec5770b97e0ce4e9ea3eeb74dfbf5d
brian <brian@xtaolabs.com>
2023-03-08 23:00:33 +0800

    :bug: Fix import

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
-from .data import cqwu_data
-from .login import login_cqwu
-from .score import score_cqwu
+import pkgutil
+from pathlib import Path
 
-__all__ = ["cqwu_data", "login_cqwu", "score_cqwu"]
+from .data import cqwu_data
+
+__all__ = ["cqwu_data"]
+
+FILE_PATH = Path(__file__).parent.absolute()
+
+for _, file, _ in pkgutil.iter_modules([str(FILE_PATH)]):
+    __import__(file, globals(), level=1)
